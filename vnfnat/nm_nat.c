20e20a42fcbc8224112c2d44f7079914d8dc8693
eden <eden@sfc.wide.ad.jp>
2014-10-24 21:46:04 +0900

    implemented base process

--- vnfnat/nm_nat.c
+++ vnfnat/nm_nat.c
@@ -14,40 +14,86 @@
 #include <netinet/ip.h>
 #include <pthread.h>
 
-#define NM_DIR_TX	0
-#define NM_DIR_RX	1
-
 struct vnfapp {
 	pthread_t tid;
 
 	int rx_fd, tx_fd;
 	int rx_q, tx_q;
-	char * rx_if, * tx_if;
-	struct netmap_ring * rx_ring, * tx_ring;
+	char *rx_if, *tx_if;
+	struct netmap_ring *rx_ring, *tx_ring;
+	unsigned int direction;
 
-	void * data;
+	void *data;
 };
 
+void nm_receive(struct vnfapp *va)
+{
+	unsigned int budget, rx_cur, tx_cur;
+	struct netmap_slot *rx_slot, *tx_slot;
+	uint32_t temp_idx;
 
-/* Process Global to Private */
-void *process_right_to_left(void * param)
+	rx_cur = va->rx_ring->cur;
+	tx_cur = va->tx_ring->cur;
+	budget = min(BUDGET_MAX, min(nm_ring_space(va->rx_ring), nm_ring_space (va->tx_ring)));
+
+	while(budget--){
+		rx_slot = &va->rx_ring->slot[rx_cur];
+		tx_slot = &va->tx_ring->slot[tx_cur];
+
+		if(tx_slot->buf_idx < 2 || rx_slot->buf_idx < 2){
+			printf("wrong index rx[%d] = %d -> tx[%d] = %d",
+				rx_cur, rx_slot->buf_idx, tx_cur, tx_slot->buf_idx);
+		}
+
+		/* NAT related process */
+		switch(va->direction){
+		case NETMAP_NAT_G2P:
+
+		case NETMAP_NAT_P2G:
+
+		default:
+		}
+
+		/* swap the buffers */
+		tmp_idx = tx_slot->buf_idx;
+		tx_slot->buf_idx = rx_slot->buf_idx;
+		rx_slot->buf_idx = tmp_idx;
+
+		/* update length */
+		tx_slot->len = rx_slot->len;
+		rx_slot->len = 0;
+
+		/* update flags */
+		tx_slot->flags = NS_BUF_CHANGED;
+		rx_slot->flags = NS_BUF_CHANGED;
+
+		rx_cur = nm_ring_next(va->rx_ring, rx_cur);
+		tx_cur = nm_ring_next(va->tx_ring, tx_cur);
+	}
+
+	/* tell the kernel to update addresses in the NIC rings */
+	va->rx_ring->head = va->rx_ring->cur = rx_cur;
+	va->tx_ring->head = va->tx_ring->cur = tx_cur;
+}
+
+void *process_netmap(void * param)
 {
 	struct vnfapp *va = (struct vnfapp *)param;
+	struct pollfd x[1];
+
+	x[0].fd = va->rx_fd;
+	x[0].events = POLLIN;
+
+	while(1){
+		poll(x, 1, -1);
+		nm_receive(va);
+	}
 
 	printf("rxfd=%d, txfd=%d, rxq=%d, txq=%d, rxif=%s, txif=%s",
 	   va->rx_fd, va->tx_fd, va->rx_q, va->tx_q, va->rx_if, va->tx_if);
 
 	return NULL;
 }
-
-/* Process Private to Global */
-void *process_left_to_right(void *param)
-{
-	struct vnfapp *va = (struct vnfapp *)param;
-
-	return NULL;
-}
-
 
 int
 nm_get_ring_num (char * ifname, int direct)
@@ -198,8 +244,9 @@ main (int argc, char ** argv)
 		va->tx_if = lif;
 		va->rx_fd = nm_vl_rx_ring (rif, va->rx_q, &va->rx_ring);
 		va->tx_fd = nm_vl_tx_ring (lif, va->tx_q, &va->tx_ring);
+		va->direction = NETMAP_NAT_G2P;
 
-		pthread_create (&va->tid, NULL, process_right_to_left, va);
+		pthread_create (&va->tid, NULL, process_netmap, va);
 	}
 
 	/* start threads from left to right */
@@ -214,8 +261,9 @@ main (int argc, char ** argv)
 		va->tx_if = rif;
 		va->rx_fd = nm_vl_rx_ring (lif, va->rx_q, &va->rx_ring);
 		va->tx_fd = nm_vl_tx_ring (rif, va->tx_q, &va->tx_ring);
+		va->direction = NETMAP_NAT_P2G;
 
-		pthread_create (&va->tid, NULL, process_left_to_right, va);
+		pthread_create (&va->tid, NULL, process_netmap, va);
 	}
 
 	pthread_join(vas[0]->tid, NULL);
---
+++ vnfnat/nm_nat.h
@@ -0,0 +1,5 @@
+#define NM_DIR_TX	0
+#define NM_DIR_RX	1
+#define NETMAP_NAT_G2P	0
+#define NETMAP_NAT_P2G	1
+#define min(a, b) ((a) < (b) ? (a) : (b))
