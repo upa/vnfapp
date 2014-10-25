c8ef561f60589d08fd9f6f23fcb3bdf53df4ddea
Ryo Nakamura <upa@haeena.net>
2014-10-26 03:57:04 +0900

    add vnfl3. L3 routing with patricia trie

--- vnfl3/vnfl3.c
+++ vnfl3/vnfl3.c
@@ -1,5 +1,5 @@
 
-/* Application VNF L3 Border (not router) */
+/* Application VNF 2 Port Router */
 
 
 #include <stdio.h>
@@ -15,25 +15,15 @@
 #include <sys/sysctl.h> /* sysctl */
 #include <ifaddrs.h>    /* getifaddrs */
 #include <net/ethernet.h>
-#include <net/if_arp.h>
 #include <netinet/in.h>
 #include <netinet/ip.h>
+#include <netinet/udp.h>
+#include <netinet/tcp.h>
 #include <pthread.h>
 
-#include "list.h"
+#include "patricia.h"
 
-
-struct arppkt {
-	struct arphdr hdr;
-
-	u_int8_t smac[ETH_ALEN];
-	struct in_addr saddr;
-	u_int8_t dmac[ETH_ALEN];
-	struct in_addr daddr;
-} __attribute__ ((__packed__));
-
-
-
+#define ACLPORT_MAX	16
 
 #define NM_DIR_TX	0
 #define NM_DIR_RX	1
@@ -41,34 +31,38 @@ struct arppkt {
 #define POLL_TIMEOUT	10
 #define BURST_MAX	1024
 
+int vale_rings = 0;
+int verbose;
+patricia_tree_t * tree;		/* routing table */
 
-#define MACCMP(a, b) \
-	(a[0] != b[0]) ? 0 : (a[1] != b[1]) ? 0 : (a[2] != b[2]) ? 0 : \
-	(a[3] != b[3]) ? 0 : (a[4] != b[4]) ? 0 : (a[5] != b[5]) ? 0 : 1
-
-#define MACCOPY(s, d)					\
-	do {						\
-		d[0] = s[0]; d[1] = s[1]; d[2] = s[2];	\
-		d[3] = s[3]; d[4] = s[4]; d[5] = s[5];	\
-	} while (0)
-
-#define BMAC(m)						\
-	do {						\
-		m[0] = 0xFF; m[1] = 0xFF; m[2] = 0xFF;	\
-		m[3] = 0xFF; m[4] = 0xFF; m[5] = 0xFF;	\
-	} while (0)
+struct vnfin {
+        int dir;
+        u_int8_t lmac[ETH_ALEN];
+        u_int8_t rmac[ETH_ALEN];
+};
 
 
-#define ADDRCMP(a, b) (*((u_int32_t *)(a)) == *((u_int32_t *)(b)))
+#define SET_R2L(v) ((v)->dir = 0)
+#define SET_L2R(v) ((v)->dir = 1)
+#define IS_R2L(v) ((v)->dir == 0)
+#define IS_L2R(v) ((v)->dir == 1)
 
-#define ADDRCOPY(s, d) (*((u_int32_t *)(s)) = *((u_int32_t *)(d)))
+#define INMAC(v) (((v)->dir == 0) ? (v)->rmac : (v)->lmac)
+
+#define OUTDSTMAC(v) (((v)->dir == 1) ? (v)->rmac : (v)->lmac)
 
 
-int verbose = 0;
+#define MACCOPY(s, d)                                   \
+        do {                                            \
+		d[0] = s[0]; d[1] = s[1]; d[2] = s[2];  \
+		d[3] = s[3]; d[4] = s[4]; d[5] = s[5];  \
+        } while (0)
 
-struct list_head arplist;
-struct netmap_ring * ltxring, * rtxring; /* ringid 0 */
-int ltx_fd, rtx_fd;
+
+#define ADDR4COPY(s, d) *(((u_int32_t *)(d))) = *(((u_int32_t *)(s)))
+#define ADDRCMP(s, d) (*(((u_int32_t *)(d))) == *(((u_int32_t *)(s))))
+
+
 
 struct vnfapp {
 	pthread_t tid;
@@ -81,219 +75,100 @@ struct vnfapp {
 	void * data;
 };
 
-/* arp entrty. It is contained in struct list_head arplist */
-struct arp {
-	struct list_head list;	/* private */
 
-	u_int8_t mac[ETH_ALEN];
-	struct in_addr addr;
+struct routeentry {
+	char out;
 };
 
 
-struct vnfl3 {
-
-	int dir;			/* left -> right or reverse */
-
-	u_int8_t rmac[ETH_ALEN];	/* mac addr of Right Port */
-	u_int8_t lmac[ETH_ALEN];	/* mac addr of Left Port */
-
-	struct in_addr raddr;		/* ip addr of Right Port */
-	struct in_addr laddr;		/* ip addr of Left Port */
-	
-	u_int8_t rdmac[ETH_ALEN];
-	u_int8_t ldmac[ETH_ALEN];
-};
-
-#define SET_R2L(v) ((v)->dir = 0)
-#define SET_L2R(v) ((v)->dir = 1)
-#define IS_R2L(v) ((v)->dir == 0)
-#define IS_L2R(v) ((v)->dir == 1)
-
-#define INMAC(v) (((v)->dir == 0) ? (v)->rmac : (v)->lmac)
-#define INADDR(v) (((v)->dir == 0) ? &((v)->raddr) : &((v)->laddr))
-
-#define OUTDSTMAC(v) (((v)->dir == 1) ? (v)->rdmac : (v)->ldmac)
-#define OUTSRCMAC(v) (((v)->dir == 1) ? (v)->rmac : (v)->lmac)
-
-#define OUTSRCADDR(v) (((v)->dir == 1) ? (v)->raddr : (v)->laddr)
-
-
-
-inline struct arp *
-find_arp_by_ip (struct list_head * arplist, struct in_addr addr)
+int
+split_prefixdst (char * str, void * prefix, int * len, char * out)
 {
-	struct arp * arp;
+	int n, l, c = 0;
+	char * p, * args[3];
 
-	list_for_each_entry (arp, arplist, list) {
-		if (ADDRCMP (&arp->addr, &addr)) {
-			return arp;
+	/* PREIFX/LEN:out */
+	
+	p = str;
+	args[c++] = str;
+	l = strlen (str);
+
+	for (n = 0; n < l; n++) {
+		if (*(p + n) == '_' || *(p + n) == '/' ||
+		    *(p + n) == ':' || *(p + n) == '-') {
+			*(p + n) = '\0';
+			args[c++] = (p + n + 1);
 		}
 	}
+
+	*len = atoi (args[1]);
+	*out = *args[2];
+
+	inet_pton (AF_INET, args[0], prefix);
+	
+	return 1;
+}
+
+
+
+static inline void
+dst2prefix (void * addr, u_int16_t len, prefix_t * prefix)
+{
+        prefix->family = AF_INET;
+        prefix->bitlen = len;
+        prefix->ref_count = 1;
+
+	ADDR4COPY (addr, &prefix->add);
+
+        return;
+}
+
+static inline void *
+find_patricia_entry (patricia_tree_t * tree, void * addr, u_int16_t len)
+{
+	prefix_t prefix;
+	patricia_node_t * pn;
+
+	dst2prefix (addr, len, &prefix);
+
+	pn = patricia_search_best (tree, &prefix);
+
+	if (pn)
+		return pn->data;
 
 	return NULL;
 }
 
-inline struct arp * 
-add_arp (struct list_head * arplist, struct in_addr addr, u_int8_t * mac)
+static inline void
+add_patricia_entry (patricia_tree_t * tree, void * addr, u_int16_t len,
+		    void * data)
 {
-	struct arp * arp;
+	prefix_t * prefix;
+	patricia_node_t * pn;
 
-	arp = (struct arp *) malloc (sizeof (struct arp));
-	memset (arp, 0, sizeof (struct arp));
+	prefix = (prefix_t *) malloc (sizeof (prefix_t));
 
-	MACCOPY (mac, arp->mac);
-	arp->addr = addr;
+	dst2prefix (addr, len, prefix);
 
-	return arp;
-}
-
-void
-send_arp_req (struct netmap_ring * txring,
-	      struct in_addr saddr, u_int8_t * smac,
-	      struct in_addr daddr)
-{
-	char pkt[64];
-	struct ether_header * eth;
-	struct arppkt * arp;
-
-	/* building arp request packet */
-	memset (pkt, 0, sizeof (pkt));
+	pn = patricia_lookup (tree, prefix);
 	
-	eth = (struct ether_header *) pkt;
-	BMAC (eth->ether_dhost);
-	MACCOPY (smac, eth->ether_shost);
-	eth->ether_type = htons (ETHERTYPE_ARP);
-
-	arp = (struct arppkt *) (eth + 1);
-	arp->hdr.ar_hrd = htons (ARPHRD_ETHER);
-	arp->hdr.ar_pro = htons (0x0800);
-	arp->hdr.ar_hln = 6;
-	arp->hdr.ar_pln = 4;
-	arp->hdr.ar_op = htons (ARPOP_REQUEST);
-
-	MACCOPY (smac, arp->smac);
-	arp->saddr = saddr;
-	arp->daddr = daddr;
-
-	/* send arp req */
-	u_int idx;
-	struct netmap_slot * slot;
-
-	idx = txring->cur;
-	slot = &txring->slot[idx];
-	
-	memcpy (NETMAP_BUF (txring, slot->buf_idx), pkt, 
-		sizeof (struct ether_header) + sizeof (struct arppkt));
-	slot->len = sizeof (struct ether_header) + sizeof (struct arppkt);
+	pn->data = data;
 
 	return;
 }
 
-void
-send_arp_rep (struct netmap_ring * txring,
-	      struct in_addr saddr, u_int8_t * smac,
-	      struct in_addr daddr, u_int8_t * dmac)
-{
-
-	char pkt[128];
-	struct ether_header * eth;
-	struct arppkt * arp;
-
-	/* building arp request packet */
-	memset (pkt, 0, sizeof (pkt));
-	
-	eth = (struct ether_header *) pkt;
-	MACCOPY (smac, eth->ether_shost);
-	MACCOPY (dmac, eth->ether_dhost);
-	eth->ether_type = htons (ETHERTYPE_ARP);
-
-	arp = (struct arppkt *) (eth + 1);
-	arp->hdr.ar_hrd = htons (ARPHRD_ETHER);
-	arp->hdr.ar_pro = htons (0x0800);
-	arp->hdr.ar_hln = 6;
-	arp->hdr.ar_pln = 4;
-	arp->hdr.ar_op = htons (ARPOP_REPLY);
-
-	MACCOPY (smac, arp->smac);
-	MACCOPY (dmac, arp->dmac);
-	arp->saddr = saddr;
-	arp->daddr = daddr;
 
 
-	D ("arp saddr %s", inet_ntoa (arp->saddr));
-	D ("arp daddr %s", inet_ntoa (arp->daddr));
-	D ("arp shost %02x:%02x:%02x:%02x:%02x:%02x",
-	   eth->ether_shost[0], eth->ether_shost[1], eth->ether_shost[2], 
-	   eth->ether_shost[3], eth->ether_shost[4], eth->ether_shost[5]);
-	D ("arp dhost %02x:%02x:%02x:%02x:%02x:%02x",
-	   eth->ether_dhost[0], eth->ether_dhost[1], eth->ether_dhost[2], 
-	   eth->ether_dhost[3], eth->ether_dhost[4], eth->ether_dhost[5]);
-
-	/* send arp rep */
-	u_int idx;
-	struct netmap_slot * slot;
-
-	idx = txring->cur;
-	slot = &txring->slot[idx];
-	
-	char * p = NETMAP_BUF (txring, slot->buf_idx);
-	memcpy (p, pkt, 
-		sizeof (struct ether_header) + sizeof (struct arppkt));
-	slot->len = sizeof (struct ether_header) + sizeof (struct arppkt);
-	
-	ioctl (ltx_fd, NIOCTXSYNC, 0);
-	ioctl (rtx_fd, NIOCTXSYNC, 0);
-
-	return;
-}
-
-inline int
-process_arp (struct vnfl3 * v,
-	     struct netmap_ring * txring, struct ether_header * eth)
-{
-	struct arppkt * arp;
-
-	D ("process arp");
-
-	arp = (struct arppkt *) (eth + 1);
-
-	if (arp->hdr.ar_op == htons (ARPOP_REQUEST)) {
-
-		if (ADDRCMP (INADDR(v), &arp->daddr)) {
-			send_arp_rep (txring,
-				      *INADDR(v), INMAC(v),
-				      arp->saddr, arp->smac);
-			return 1;
-		}
-
-	} else if (arp->hdr.ar_op == htons (ARPOP_REPLY)) {
-
-		if (find_arp_by_ip (&arplist, arp->saddr) == NULL)
-			add_arp (&arplist, arp->saddr, arp->smac);
-
-		return 1;
-	}
-
-	return 0;
-}
 
 u_int
 move (struct vnfapp * va)
 {
-	int arp_done = 0;
-	u_int burst, m, idx, j, k;
-	struct ether_header * eth;
+	u_int burst, m, j, k;
+	struct vnfin * v = va->data;
 	struct netmap_slot * rx_slot, * tx_slot;
-	struct vnfl3 * v = va->data;
-	struct arp * arp;
+	struct ether_header * eth;
 	struct ip * ip;
-	struct netmap_ring * arp_txring;
-
-	if (IS_L2R(v)) {
-		arp_txring = ltxring;
-	} else {
-		arp_txring = rtxring;
-	}
+	struct routeentry * route;
 
 	j = va->rx_ring->cur;
 	k = va->tx_ring->cur;
@@ -321,40 +196,31 @@ move (struct vnfapp * va)
 			  j, rx_slot->buf_idx, k, tx_slot->buf_idx);
                         sleep(2);
                 }
+
+		eth = (struct ether_header *)
+			NETMAP_BUF (va->rx_ring, rx_slot->buf_idx);
+		ip = (struct ip *) (eth + 1);
+
+
+		/* route entry check */
+		route = find_patricia_entry (tree, &ip->ip_dst, 32);
+		if (!route) {
+			goto drop;
+		}
+		if (!(IS_L2R (v) && route->out == 'r') &&
+		    !(IS_R2L (v) && route->out == 'l')) {
+			goto drop;
+		}
+
 		
+		/* change destination mac */
+#ifdef ZEROCPY
+		u_int idx;
 		eth = (struct ether_header *)
 			NETMAP_BUF (va->rx_ring, rx_slot->buf_idx);
 
-		if (eth->ether_type == htons (ETHERTYPE_ARP)) {
-			if (process_arp (v, arp_txring, eth)) {
-				goto next_burst;
-			}
-		}
+		MACCOPY (OUTDSTMAC(v), eth->ether_dhost);
 
-		/* change mac address for L3 Boundry */
-		if (IS_R2L (v)) {
-			/* to normal network. resolve arp */
-			if (eth->ether_type == htons (ETHERTYPE_IP)) {
-				ip = (struct ip *) (eth + 1);
-				arp = find_arp_by_ip (&arplist, ip->ip_dst);
-				if (arp == NULL && arp_done == 0) {
-					send_arp_req (va->tx_ring,
-						      OUTSRCADDR(v),
-						      OUTSRCMAC(v),
-						      ip->ip_dst);
-					arp_done = 1;
-					goto next_burst;
-				}
-				MACCOPY (OUTSRCMAC(v), eth->ether_shost);
-				MACCOPY (arp->mac, eth->ether_dhost);
-			}
-		} else {
-			/* to nfv chain (mac is no cair )*/
-			MACCOPY (OUTDSTMAC(v), eth->ether_dhost);
-			MACCOPY (OUTSRCMAC(v), eth->ether_shost);
-		}
-
-		/* swap slot */
 		idx = tx_slot->buf_idx;
 		tx_slot->buf_idx = rx_slot->buf_idx;
 		rx_slot->buf_idx = idx;
@@ -362,7 +228,19 @@ move (struct vnfapp * va)
 		rx_slot->flags |= NS_BUF_CHANGED;
 		tx_slot->len = rx_slot->len;
 
-	next_burst:
+#else
+                char * spkt = NETMAP_BUF (va->rx_ring, rx_slot->buf_idx);
+                char * dpkt = NETMAP_BUF (va->tx_ring, tx_slot->buf_idx);
+                nm_pkt_copy (spkt, dpkt, rx_slot->len);
+
+                /* change destination mac */
+                eth = (struct ether_header *) dpkt;
+                MACCOPY (OUTDSTMAC(v), eth->ether_dhost);
+                tx_slot->len = rx_slot->len;
+#endif
+
+
+	drop:
 		j = nm_ring_next (va->rx_ring, j);
 		k = nm_ring_next (va->tx_ring, k);
 	}
@@ -392,6 +270,7 @@ processing_hub (struct vnfapp * va)
 
 		move (va);
 		ioctl (va->tx_fd, NIOCTXSYNC, va->tx_q);
+
 	}
 
 	return;
@@ -431,6 +310,12 @@ nm_get_ring_num (char * ifname, int direct)
 	memset (&nmr, 0, sizeof (nmr));
 	nmr.nr_version = NETMAP_API;
 	strncpy (nmr.nr_name, ifname, IFNAMSIZ - 1);
+
+        if (vale_rings && strncmp (ifname, "vale", 4) == 0) {
+                nmr.nr_rx_rings = vale_rings;
+                nmr.nr_tx_rings = vale_rings;
+        }
+
 	if (ioctl (fd, NIOCGINFO, &nmr)) {
 		D ("unable to get interface info for %s", ifname);
 		return -1;
@@ -478,6 +363,11 @@ nm_ring (char * ifname, int q, struct netmap_ring ** ring,  int x, int w)
 		return -1;
 	}
 
+        if (vale_rings && strncmp (ifname, "vale", 4) == 0) {
+                nmr.nr_rx_rings = vale_rings;
+                nmr.nr_tx_rings = vale_rings;
+        }
+
 	mem = mmap (NULL, nmr.nr_memsize,
 		    PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0);
 	if (mem == MAP_FAILED) {
@@ -503,14 +393,14 @@ nm_ring (char * ifname, int q, struct netmap_ring ** ring,  int x, int w)
 
 void
 usage (void) {
-
 	printf ("-l [LEFT] -r [RIGHT] -q [CPUNUM] (-v)\n"
-		"-a [LMAC] -A [RMAC] -b [LADDR] -B [RADDR]\n"
+		"-L [LEFTOUTMAC] -R[RIGHTOUTMAC]\n"
+		"-f [PREFIX/LEN:OUT] -f ... -f...\n"
+		"-e [VALERINGNUM]\n"
 		"\n"
-		"LEFT interface is connected to Normal network.\n"
-		"RIGHT interface is connected to NFV network\n"
-		"It means, when packet is transmitted to LEFT, "
-		"ARP will be resolved\n\n");
+		"port number 0 means all\n"
+		"out is 'l' or 'r'\n"
+		);
 
 	return;
 }
@@ -520,16 +410,25 @@ usage (void) {
 int
 main (int argc, char ** argv)
 {
-	int q, rq, lq, n, ch, mac[ETH_ALEN];
+	int ret, q, rq, lq, n, ch, mac[ETH_ALEN];
 	char * rif, * lif;	/* right/left interfaces */
+	struct vnfin vi;
+
+	int len;
+	char out;
+	struct in_addr prefix;
+	struct routeentry * route;
+
 
 	q = 256;	/* all CPUs */
 	rif = lif = NULL;
+	verbose = 0;
 
-	struct vnfl3 v3;
-	memset (&v3, 0, sizeof (v3));
+	tree = New_Patricia (32);
 
- 	while ((ch = getopt (argc, argv, "r:l:q:va:A:b:B:")) != -1) {
+	memset (&vi, 0, sizeof (vi));
+
+	while ((ch = getopt (argc, argv, "r:l:q:R:L:f:e:")) != -1) {
 		switch (ch) {
 		case 'r' :
 			rif = optarg;
@@ -543,23 +442,50 @@ main (int argc, char ** argv)
 		case 'v' :
 			verbose = 1;
 			break;
-		case 'a' :
+		case 'L' :
 			sscanf (optarg, "%02x:%02x:%02x:%02x:%02x:%02x", 
 				&mac[0], &mac[1], &mac[2],
 				&mac[3], &mac[4], &mac[5]);
-			MACCOPY (mac, v3.lmac);
+			MACCOPY (mac, vi.lmac);
 			break;
-		case 'A' :
+		case 'R' :
 			sscanf (optarg, "%02x:%02x:%02x:%02x:%02x:%02x", 
 				&mac[0], &mac[1], &mac[2],
 				&mac[3], &mac[4], &mac[5]);
-			MACCOPY (mac, v3.rmac);
+			MACCOPY (mac, vi.rmac);
 			break;
-		case 'b' :
-			inet_pton (AF_INET, optarg, &v3.laddr);
+		case 'f' :
+
+			D ("install Route Entry %s", optarg);
+
+			ret = split_prefixdst (optarg, &prefix, &len, &out);
+
+			if (!ret) {
+				D ("invalid route %s\n", optarg);
+				return -1;
+			}
+
+			if (out != 'r' && out != 'l') {
+				D ("invalid output %s\n", optarg);
+				return -1;
+			}
+
+			/* install route  */
+			route = (struct routeentry *)
+				malloc (sizeof (struct routeentry));
+			memset (route, 0, sizeof (struct routeentry));
+			route->out = out;
+
+			add_patricia_entry (tree, &prefix, len, route);
+
 			break;
-		case 'B' :
-			inet_pton (AF_INET, optarg, &v3.raddr);
+			
+		case 'e' :
+			vale_rings = atoi (optarg);
+			if (vale_rings > 4) {
+				D ("vale ring should be smaller than 4");
+				return -1;
+			}
 			break;
 		default :
 			usage ();
@@ -580,14 +506,12 @@ main (int argc, char ** argv)
 		return -1;
 	}
 	D ("rq=%d, lq=%d", rq, lq);
-
-	D ("Left Interface %02d:%02d:%02d:%02d:%02d:%02d and %s", 
-	   v3.lmac[0], v3.lmac[1], v3.lmac[3],
-	   v3.lmac[3], v3.lmac[4], v3.lmac[5], inet_ntoa (v3.laddr));
-
-	D ("Right Interface %02d:%02d:%02d:%02d:%02d:%02d and %s", 
-	   v3.rmac[0], v3.rmac[1], v3.rmac[3],
-	   v3.rmac[3], v3.rmac[4], v3.rmac[5], inet_ntoa (v3.raddr));
+	D ("Change Mac Left %02x:%02x:%02x:%02x:%02x:%02x <-> "
+	   "%02x:%02x:%02x:%02x:%02x:%02x",
+	   vi.lmac[0], vi.lmac[1], vi.lmac[2], 
+	   vi.lmac[3], vi.lmac[4], vi.lmac[5],
+	   vi.rmac[0], vi.rmac[1], vi.rmac[2], 
+	   vi.rmac[3], vi.rmac[4], vi.rmac[5]);
 
 	/* asign processing threads */
 
@@ -600,12 +524,11 @@ main (int argc, char ** argv)
 		va = (struct vnfapp *) malloc (sizeof (struct vnfapp));
 		memset (va, 0, sizeof (struct vnfapp));
 
-		struct vnfl3 * v;
-		v = (struct vnfl3 *) malloc (sizeof (struct vnfl3));
-		memcpy (v, &v3, sizeof (struct vnfl3));
+		struct vnfin * v;
+		v = (struct vnfin *) malloc (sizeof (struct vnfin));
+		memcpy (v, &vi, sizeof (struct vnfin));
 
 		SET_R2L (v);
-
 		va->data = v;
 		va->rx_q = n;
 		va->tx_q = n % lq;
@@ -613,11 +536,6 @@ main (int argc, char ** argv)
 		va->tx_if = lif;
 		va->rx_fd = nm_vl_rx_ring (rif, va->rx_q, &va->rx_ring);
 		va->tx_fd = nm_vl_tx_ring (lif, va->tx_q, &va->tx_ring);
-
-		if (n == 0) {
-			ltxring = va->tx_ring;
-			ltx_fd = va->tx_fd;
-		}
 
 		pthread_create (&va->tid, NULL, processing_thread, va);
 	}
@@ -628,12 +546,11 @@ main (int argc, char ** argv)
 		va = (struct vnfapp *) malloc (sizeof (struct vnfapp));
 		memset (va, 0, sizeof (struct vnfapp));
 
-		struct vnfl3 * v;
-		v = (struct vnfl3 *) malloc (sizeof (struct vnfl3));
-		memcpy (v, &v3, sizeof (struct vnfl3));
+		struct vnfin * v;
+		v = (struct vnfin *) malloc (sizeof (struct vnfin));
+		memcpy (v, &vi, sizeof (struct vnfin));
 
 		SET_L2R (v);
-
 		va->data = v;
 		va->rx_q = n;
 		va->tx_q = n % rq;
@@ -641,11 +558,6 @@ main (int argc, char ** argv)
 		va->tx_if = rif;
 		va->rx_fd = nm_vl_rx_ring (lif, va->rx_q, &va->rx_ring);
 		va->tx_fd = nm_vl_tx_ring (rif, va->tx_q, &va->tx_ring);
-
-		if (n == 0) {
-			rtxring = va->tx_ring;
-			rtx_fd = va->tx_fd;
-		}
 
 		pthread_create (&va->tid, NULL, processing_thread, va);
 	}
